6a671763529c5980f3180edd9638d0111847ded3
Jennaleigh Angala <jennayey@gmail.com>
2020-07-11 18:13:21 +0800

    Hanunuo Update
    Added Dropdown for Language selection
    Added Hanunuo Script to supported languages

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-function showTooltip () {
-  let toast = document.getElementById('toast')
+window.onload = function () {
+  document.getElementById("translatedTextArea").classList.toggle("baybayin");
+};
+function showTooltip() {
+  let toast = document.getElementById("toast");
 
-  toast.classList.add('show')
-  setTimeout (
-    function () {
-      toast.classList.remove ('show')
-    }, 2100
-  )
+  toast.classList.add("show");
+  setTimeout(function () {
+    toast.classList.remove("show");
+  }, 2100);
 }
 
 function clearText() {
@@ -17,23 +18,22 @@ function clearText() {
   // goBack()
 }
 
-function copyText () {
+function copyText() {
   let translatedText = document.getElementById("translatedTextArea");
-translatedText.select()
-// translatedText.
-document.execCommand("copy")
+  translatedText.select();
+  // translatedText.
+  document.execCommand("copy");
 
-showTooltip()
+  showTooltip();
 }
 
-function focus () {
-  let rawText = document.getElementById("tagalogTextArea")
-rawText.focus()
+function focus() {
+  let rawText = document.getElementById("tagalogTextArea");
+  rawText.focus();
 }
-function convertText() {
+
+function baybayinTranslate() {
   let rawText = document.getElementById("tagalogTextArea").value.toLowerCase();
-  let translatedText = document.getElementById("translatedTextArea");
-  // sessionStorage.setItem("rText", rawText)
   rawText = rawText.replace(/o/g, "u");
   rawText = rawText.replace(/e/g, "i");
 
@@ -107,5 +107,133 @@ function convertText() {
   rawText = rawText.replace(/i/g, "\u1701");
   rawText = rawText.replace(/u/g, "\u1702");
 
-  translatedText.value = rawText;
+  return rawText;
+}
+
+function hanunuoTranslate() {
+  let rawText = document.getElementById("tagalogTextArea").value.toLowerCase();
+  rawText = rawText.replace(/o/g, "u");
+  rawText = rawText.replace(/e/g, "i");
+
+  rawText = rawText.replace(/nga/g, "\u1725");
+  rawText = rawText.replace(/ngi/g, "\u1725\u1732");
+  rawText = rawText.replace(/ngu/g, "\u1725\u1733");
+  rawText = rawText.replace(/ng/g, "\u1725\u1734");
+
+  rawText = rawText.replace(/ka/g, "\u1723");
+  rawText = rawText.replace(/ga/g, "\u1724");
+  rawText = rawText.replace(/ta/g, "\u1726");
+  rawText = rawText.replace(/da/g, "\u1727");
+  rawText = rawText.replace(/na/g, "\u1728");
+  rawText = rawText.replace(/pa/g, "\u1729");
+  rawText = rawText.replace(/ba/g, "\u172A");
+  rawText = rawText.replace(/ma/g, "\u172B");
+  rawText = rawText.replace(/ya/g, "\u172C");
+  rawText = rawText.replace(/ra/g, "\u172D");
+  rawText = rawText.replace(/la/g, "\u172E");
+  rawText = rawText.replace(/wa/g, "\u172F");
+  rawText = rawText.replace(/sa/g, "\u1730");
+  rawText = rawText.replace(/ha/g, "\u1731");
+
+  rawText = rawText.replace(/ki/g, "\u1723\u1732");
+  rawText = rawText.replace(/gi/g, "\u1724\u1732");
+  rawText = rawText.replace(/ti/g, "\u1726\u1732");
+  rawText = rawText.replace(/di/g, "\u1727\u1732");
+  rawText = rawText.replace(/ni/g, "\u1728\u1732");
+  rawText = rawText.replace(/pi/g, "\u1729\u1732");
+  rawText = rawText.replace(/bi/g, "\u172A\u1732");
+  rawText = rawText.replace(/mi/g, "\u172B\u1732");
+  rawText = rawText.replace(/yi/g, "\u172C\u1732");
+  rawText = rawText.replace(/ri/g, "\u172D\u1732");
+  rawText = rawText.replace(/li/g, "\u172E\u1732");
+  rawText = rawText.replace(/wi/g, "\u172F\u1732");
+  rawText = rawText.replace(/si/g, "\u1730\u1732");
+  rawText = rawText.replace(/hi/g, "\u1731\u1732");
+
+  rawText = rawText.replace(/ku/g, "\u1723\u1733");
+  rawText = rawText.replace(/gu/g, "\u1724\u1733");
+  rawText = rawText.replace(/tu/g, "\u1726\u1733");
+  rawText = rawText.replace(/du/g, "\u1727\u1733");
+  rawText = rawText.replace(/nu/g, "\u1728\u1733");
+  rawText = rawText.replace(/pu/g, "\u1729\u1733");
+  rawText = rawText.replace(/bu/g, "\u172A\u1733");
+  rawText = rawText.replace(/mu/g, "\u172B\u1733");
+  rawText = rawText.replace(/yu/g, "\u172C\u1733");
+  rawText = rawText.replace(/ru/g, "\u172D\u1733");
+  rawText = rawText.replace(/lu/g, "\u172E\u1733");
+  rawText = rawText.replace(/wu/g, "\u172F\u1733");
+  rawText = rawText.replace(/su/g, "\u1730\u1733");
+  rawText = rawText.replace(/hu/g, "\u1731\u1733");
+
+  rawText = rawText.replace(/k/g, "\u1723\u1734");
+  rawText = rawText.replace(/g/g, "\u1724\u1734");
+
+  rawText = rawText.replace(/t/g, "\u1726\u1734");
+  rawText = rawText.replace(/d/g, "\u1727\u1734");
+  rawText = rawText.replace(/n/g, "\u1728\u1734");
+  rawText = rawText.replace(/p/g, "\u1729\u1734");
+  rawText = rawText.replace(/b/g, "\u172A\u1734");
+  rawText = rawText.replace(/m/g, "\u172B\u1734");
+  rawText = rawText.replace(/y/g, "\u172C\u1734");
+  rawText = rawText.replace(/r/g, "\u172D\u1734");
+  rawText = rawText.replace(/l/g, "\u172E\u1734");
+  rawText = rawText.replace(/w/g, "\u172F\u1734");
+  rawText = rawText.replace(/s/g, "\u1730\u1734");
+  rawText = rawText.replace(/h/g, "\u1731\u1734");
+
+  rawText = rawText.replace(/a/g, "\u1720");
+  rawText = rawText.replace(/i/g, "\u1721");
+  rawText = rawText.replace(/u/g, "\u1722");
+
+  return rawText;
+}
+
+function convertText() {
+  let translatedText = document.getElementById("translatedTextArea");
+  let languageSelected = document.getElementById("languageSelected").innerHTML;
+  console.log(languageSelected.substring("10").trim());
+  switch (languageSelected.substring("10").trim()) {
+    case "Baybayin":
+      translatedText.value = baybayinTranslate();
+      break;
+    case "Hanunuo":
+      translatedText.value = hanunuoTranslate();
+      break;
+    case "":
+      translatedText.value = baybayinTranslate();
+
+    default:
+      translatedText.value = baybayinTranslate();
+  }
+}
+
+function showDropdown() {
+  //toggles the dropdown
+  document.getElementById("dropdownContents").classList.toggle("show");
+}
+
+function changeLanguage(language) {
+  let languageChoices = document.getElementById("dropdownContents").children;
+  // console.log(language.id);
+  //removes former selected
+  let translatedText = document.getElementById("translatedTextArea");
+
+  for (a = 0; a < languageChoices.length; a++) {
+    if (languageChoices[a].classList.contains("selected")) {
+      languageChoices[a].classList.toggle("selected");
+    }
+  }
+
+  //sets the button text to new
+  document.getElementById(
+    "languageSelected"
+  ).innerHTML = document.getElementById(language.id).innerHTML;
+  //toggles the selected class to new chosen one
+  document.getElementById(language.id).classList.toggle("selected");
+  translatedText.className = "";
+  translatedText.classList.toggle(language.id);
+  //closes the dropdown
+  showDropdown();
+  //changes language on switch of language
+  convertText();
 }
